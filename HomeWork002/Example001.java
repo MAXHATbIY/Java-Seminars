13785ef75cf466a73592491d9b74a5a1f038d2fa
Slava <mc_slava2@mail.ru>
2023-03-12 15:38:14 +0300

    add HW2EX2

--- HomeWork002/Example001.java
+++ HomeWork002/Example001.java
@@ -11,6 +11,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+// Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
+// Если значение null, то параметр не должен попадать в запрос.
+
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
 
 public class Example001 {
     public static String reader() {
